ef6ed0aa73883cb6ad5126c78c63ad52c2d8a37a
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2012-12-12 09:25:45 +0100

    init of cob_camera_alignment

---
+++ cob_camera_alignment/ros/src/calib_node.cpp
@@ -0,0 +1,307 @@
+/*
+ * calib_node.cpp
+ *
+ *  Created on: 05.12.2012
+ *      Author: josh
+ */
+
+
+
+#include <ros/ros.h>
+#include <pcl_ros/pcl_nodelet.h>
+#include <tf/transform_listener.h>
+#include <XmlRpcException.h>
+
+#include <actionlib/server/simple_action_server.h>
+#include <cob_camera_alignment/StartMeasurementsAction.h>
+
+#include <cob_3d_mapping_msgs/ShapeArray.h>
+#include <cob_3d_shapes/conversion.h>
+
+
+class As_Node
+{
+protected:
+  ros::NodeHandle n_;
+  tf::TransformListener tf_listener_;
+public:
+  As_Node(): n_("~") {
+  }
+
+  virtual ~As_Node() {}
+
+  virtual void onInit()=0;
+
+  void start() {
+
+  }
+};
+
+class As_Nodelet : public  pcl_ros::PCLNodelet
+{
+protected:
+  ros::NodeHandle n_;
+public:
+  As_Nodelet() {
+  }
+
+  virtual ~As_Nodelet() {}
+
+  void start() {
+    PCLNodelet::onInit();
+    n_ = getNodeHandle();
+  }
+};
+
+template <typename Parent>
+class Calib_Node : public Parent
+{
+
+  typedef actionlib::SimpleActionServer<cob_camera_alignment::StartMeasurementsAction> ActionServer;
+
+  ros::Subscriber shapes_sub_;
+
+  static bool sort_shapes_by_weight(const cob_3d_mapping_msgs::Shape &a, const cob_3d_mapping_msgs::Shape &b) {
+    return a.weight > b.weight;
+  }
+
+  Eigen::Matrix3f getAlignment(const Eigen::Matrix3f &expection, const boost::shared_ptr<cob_3d_shapes::Surface> &a, const boost::shared_ptr<cob_3d_shapes::Surface> &b, const ros::Time &time) {
+    ROS_ASSERT_MSG(a, "cannot read this shape type");
+    ROS_ASSERT_MSG(b, "cannot read this shape type");
+
+    //std::cout<<a->normalUV(Eigen::Vector2f::Zero())<<"\n";
+
+    Eigen::Vector3f n1 = a->normalNormalized(Eigen::Vector2f::Zero());
+    Eigen::Vector3f n2 = b->normalNormalized(Eigen::Vector2f::Zero());
+
+    Eigen::Matrix3f R, Rbest, Rtmp;
+    R.col(0) = n1;
+    R.col(1) = n2;
+    R.col(2) = R.col(0).cross(R.col(1));
+
+    Eigen::Vector3f x,y;
+    x.fill(0);y=x;
+    x(0)=1;y(1)=1;
+
+    float qual_best = std::numeric_limits<float>::max();
+    for(int i=0; i<4; i++)
+      for(int j=0; j<4; j++)
+      {
+        Eigen::AngleAxisf aa1(M_PI/2*i, x), aa2(M_PI/2*j, y);
+        Rtmp = aa1*aa2*R;
+        float qual = (expection * Rtmp.transpose()).diagonal().squaredNorm();
+        if(qual<qual_best) {
+          qual_best = qual;
+          Rbest = Rtmp;
+        }
+      }
+
+    return Rbest;
+  }
+
+  float getAlignment2(const boost::shared_ptr<cob_3d_shapes::Surface> &a, const Eigen::Vector3f &axis) {
+    ROS_ASSERT_MSG( std::abs(1-axis.squaredNorm())<0.001f, "axis have to be normalized" );
+    float d = axis.dot( a->normalNormalized(Eigen::Vector2f::Zero()) );
+    if(d<0) return -std::acos( -d );
+    return std::acos( d );
+  }
+
+  struct AVERAGE {
+    float delta_;
+    int num_;
+
+    AVERAGE():delta_(0),num_(0) {}
+
+    void operator+=(const float d) {
+      ++num_;
+      delta_+=d;
+    }
+
+    void clear() {
+      delta_=0;
+      num_=0;
+    }
+
+    operator float() {
+      return delta_/num_;
+    }
+  };
+  std::vector<Eigen::Vector3f> normals_;
+  std::vector<AVERAGE> delta_;
+  std::string tf_, world_;
+  float threshold_;
+  ActionServer as_;
+  cob_camera_alignment::StartMeasurementsFeedback feedback_;
+  cob_camera_alignment::StartMeasurementsResult result_;
+  int needed_;
+
+public:
+  Calib_Node():
+    as_(this->n_, "/start_measurements", false), needed_(0)
+  {}
+
+  void onInit()
+  {
+    threshold_ = 0.1f;
+
+    ros::NodeHandle nh_("~");
+
+    if (nh_.hasParam("walls"))
+    {
+      try {
+        XmlRpc::XmlRpcValue v;
+        nh_.param("walls", v, v);
+        for(int i =0; i < v.size(); i++)
+        {
+          ROS_ASSERT(v[i].size()==3);
+          Eigen::Vector3f n;
+
+          n(0) = (double)v[i][0];
+          n(1) = (double)v[i][1];
+          n(2) = (double)v[i][2];
+
+          normals_.push_back(n);
+          delta_.push_back(AVERAGE());
+        }
+      }
+      catch(XmlRpc::XmlRpcException &e) {
+        ROS_ERROR("error parsing yaml for walls\n%s\nshutdown...", e.getMessage().c_str());
+        nh_.shutdown();
+        return;
+      }
+      catch(...) {
+        ROS_ERROR("error parsing yaml for walls, shutdown...");
+        nh_.shutdown();
+        return;
+      }
+    }
+    else
+    {
+      ROS_ERROR("Parameter walls not set, shutting down node...");
+      nh_.shutdown();
+      return;
+    }
+
+    if (nh_.hasParam("tf"))
+    {
+      nh_.getParam("tf",tf_);
+    }
+    else
+    {
+      ROS_ERROR("Parameter tf not set, shutting down node...");
+      nh_.shutdown();
+      return;
+    }
+
+    if (nh_.hasParam("world"))
+    {
+      nh_.getParam("world",world_);
+    }
+    else
+    {
+      ROS_ERROR("Parameter world not set, shutting down node...");
+      nh_.shutdown();
+      return;
+    }
+
+    if (nh_.hasParam("threshold"))
+    {
+      double t;
+      nh_.getParam("threshold",t);
+      threshold_ = (float)t;
+    }
+
+    shapes_sub_ = this->n_.subscribe("/shapes_array", 1, &Calib_Node<Parent>::cbShapes, this);
+
+    //register the goal and feeback callbacks
+    as_.registerGoalCallback(boost::bind(&Calib_Node<Parent>::goalCB, this));
+    as_.registerPreemptCallback(boost::bind(&Calib_Node<Parent>::preemptCB, this));
+    as_.start();
+  }
+
+  void goalCB()
+  {
+    for(size_t i=0; i<delta_.size(); i++)
+      delta_[i].clear();
+    needed_ = as_.acceptNewGoal()->number_of_frames;
+  }
+
+  void preemptCB()
+  {
+    needed_ = 0;
+  }
+
+  void
+  cbShapes(cob_3d_mapping_msgs::ShapeArray::ConstPtr cpa)
+  {
+    if(cpa->shapes.size()<1 || needed_<1) return;
+
+    std::vector<cob_3d_mapping_msgs::Shape> cp = cpa->shapes;
+    std::sort(cp.begin(), cp.end(), sort_shapes_by_weight);
+
+    tf::StampedTransform transform;
+    try{
+      this->tf_listener_.lookupTransform(world_, tf_,
+                                         cpa->header.stamp, transform);
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+    }
+
+    Eigen::Quaternionf q;
+    q.x() = transform.getRotation().getX();
+    q.y() = transform.getRotation().getY();
+    q.z() = transform.getRotation().getZ();
+    q.w() = transform.getRotation().getW();
+
+    int n=0;
+    for(size_t i=0; i<normals_.size(); i++) {
+
+      for(size_t j=0; j<cp.size(); j++) {
+        const float d = getAlignment2(cob_3d_shapes::SurfaceFromShapeMsg(cp[j]), q*normals_[i]);
+        if(d>threshold_) {
+          ROS_WARN("delta angle between axis %d is %f",(int)i,d );
+          continue;
+        }
+
+        ROS_INFO("delta angle between axis %d is %f",(int)i,d );
+        delta_[i] += d;
+        ++n;
+        break;
+      }
+    }
+
+    if(n==(int)normals_.size()) {
+      feedback_.number_of_measurements = --needed_;
+      as_.publishFeedback(feedback_);
+      if(needed_==0) {
+        for(size_t i=0; i<delta_.size(); i++)
+          result_.deltas.push_back( (float)delta_[i] );
+        as_.setSucceeded(result_);
+      }
+    }
+
+    /*Eigen::Matrix3f R = getAlignment(Eigen::Matrix3f::Identity(), cob_3d_shapes::SurfaceFromShapeMsg(cp[0]), cob_3d_shapes::SurfaceFromShapeMsg(cp[1]), cpa->header.stamp);
+
+    std::cout<<R<<"\n";
+
+    for(size_t i=0; i<axis_.size(); i++) {
+      const float d = std::acos( (R*axis_[i]).dot(axis_[i]) );
+      ROS_INFO("delta angle between axis %d is %f",(int)i,d );
+      delta_[i] += d;
+    }*/
+  }
+
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "camera_alignment");
+
+  Calib_Node<As_Node> sn;
+  sn.onInit();
+
+  ros::spin();
+
+  return 0;
+}
---
+++ cob_camera_alignment/ros/src/start_calib.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python
+#################################################################
+##\file
+#
+# \note
+#   Copyright (c) 2010 \n
+#   Fraunhofer Institute for Manufacturing Engineering
+#   and Automation (IPA) \n\n
+#
+#################################################################
+#
+# \note
+#   Project name: care-o-bot
+# \note
+#   ROS stack name: cob_environment_perception_intern
+# \note
+#   ROS package name: cob_3d_mapping_demonstrator
+#
+# \author
+#   Author: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+# \author
+#   Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+#
+# \date Date of creation: 03/2012
+#
+# \brief
+#   Implementation of ROS node for script_server.
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer. \n
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution. \n
+#     - Neither the name of the Fraunhofer Institute for Manufacturing
+#       Engineering and Automation (IPA) nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL along with this program.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################
+
+import time
+
+import roslib
+roslib.load_manifest('cob_camera_alignment')
+import rospy
+import actionlib
+import yaml
+
+from cob_camera_alignment.msg import *
+from cob_srvs.srv import *
+from simple_script_server import *
+
+sss = simple_script_server()
+
+## Script server class which inherits from script class.
+#
+# Implements actionlib interface for the script server.
+#
+class execute_button_commands():
+  ## Initializes the actionlib interface of the script server.
+  #
+  def __init__(self):
+    self.ns_global_prefix = "/cob_calibration"
+
+    self.no_of_fr = rospy.get_param('~number_of_frames', 20)
+    self.var = rospy.get_param('~variance')
+    self.max_delta = rospy.get_param('~max_delta_rad')
+
+    self.actor = rospy.get_param('~actor')
+    self.default_joints = rospy.get_param('~default_joints')
+    self.joint_index = rospy.get_param('~joint_index')
+
+    self.step_size = rospy.get_param('~step_size')
+    self.range_min = rospy.get_param('~range_min')
+    self.range_max = rospy.get_param('~range_max')
+
+    self.script_action_server = actionlib.SimpleActionServer("cob_calibration", ScriptAction, self.execute_cb, False)
+    self.script_action_server.register_preempt_callback(self.execute_stop)
+    self.script_action_server.start()
+    self.trigger_client = actionlib.SimpleActionClient('/start_measurements', StartMeasurementsAction)
+
+    rospy.Service('/cob_calibration_controller/nullposition', Trigger, self.execute_nullposition)
+    rospy.Service('/cob_calibration_controller/movesteps', Trigger, self.execute_movesteps)
+
+#------------------- Actionlib section -------------------#
+  ## Executes actionlib callbacks.
+  #
+  # \param server_goal ScriptActionGoal
+  #
+  def execute_cb(self, server_goal):
+    server_result = ScriptActionResult().result
+    if server_goal.function_name == "nullposition":
+      ret=self.execute_nullposition()
+    elif server_goal.function_name == "stop":
+      ret=self.execute_stop()
+    else:
+      rospy.logerr("function <<%s>> not supported", server_goal.function_name)
+      self.script_action_server.set_aborted(server_result)
+      return
+
+    #server_result.error_code = handle01.get_error_code()
+    #if server_result.error_code == 0:
+    #  rospy.logdebug("action result success")
+    if self.script_action_server.is_active():
+      self.script_action_server.set_succeeded(server_result)
+    #else:
+    #  self.script_action_server.set_aborted(server_result)
+    #else:
+    #  rospy.logerr("action result error")
+    #  self.script_action_server.set_aborted(server_result)
+
+  def move_to(self, pos):
+    j = self.default_joints
+    j[self.joint_index] = pos
+    #sss.move(self.actor,[j])
+
+  def measure(self):
+    goal = StartMeasurementsGoal()
+    goal.number_of_frames = self.no_of_fr
+    self.trigger_client.send_goal(goal)
+    if not self.trigger_client.wait_for_result(rospy.Duration.from_sec(1.0*goal.number_of_frames)):
+      print "measurement failed"
+      return 100
+    res = self.trigger_client.get_result()
+    if len(res.deltas)<1:
+      print "measurement invalid"
+      return 100
+    return res.deltas[0]
+
+  def measure_at(self, pos):
+    self.move_to(pos)
+    return self.measure()
+
+  def iterate1(self, mid, r):
+    print r
+    if r<self.var: return [mid+r/2, self.measure_at(mid+r/2)]
+
+    d = [self.measure_at(mid-r), self.measure_at(mid+r)]
+    if abs(d[0])<abs(d[1]):
+       return self.iterate1(mid-r/2,r/2+self.var)
+    else:
+       return self.iterate1(mid+r/2,r/2+self.var*0.1)
+
+
+
+  def execute_nullposition(self, req):
+    print "calibrating null-position..."
+    d = self.iterate1(0,self.max_delta)
+    print "null-position at ",d[0]
+    print "literal difference ",d[1]," rad"
+    return TriggerResponse()
+
+  def execute_stop(self):
+    print "stop"
+    self.script_action_server.set_preempted()
+    sss.stop("cob_3d_mapping_demonstrator")
+
+
+
+  def execute_movesteps(self, req):
+    print "calibrating null-position..."
+    m = {}
+    for s in [x * self.step_size for x in range(self.range_min, self.range_max)]:
+    	d=self.measure_at(s)
+    	m[s]=d
+    	print "deviation at ",s," is ",d," rad"
+    print m
+    return TriggerResponse()
+
+  def execute_stop(self):
+    print "stop"
+    self.script_action_server.set_preempted()
+    sss.stop("cob_3d_mapping_demonstrator")
+
+## Main routine for running the script server
+#
+if __name__ == '__main__':
+  rospy.init_node('start_calib')
+  execute_button_commands()
+  rospy.loginfo("calibration is ready")
+  rospy.spin()
